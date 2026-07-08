1832a29e0e53c7e9fb0eaaf336afb93d99259f40
Apparats <raidermonsax@gmail.com>
2026-07-08 14:38:45 -0400

    Golden Warerita

---
+++ expansions/base/GoldenWarerita.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "GoldenWarerita",
+  cost: 3,
+  type: "minion",
+  attack: 2,
+  health: 6,
+  keywords: [],
+  race: "Monster",
+  rarity: "mythic",
+  country: "Arcana",
+  lore: "Golden Warerita! After dying, it turns into a normal warerita.",
+  image: "art/GoldenWarerita.webp",
+  abilities: [
+    { trigger: "onDeath", effect: "summonMinion", cardId: "base:Warerita", count: 1 },
+  ],
+};
