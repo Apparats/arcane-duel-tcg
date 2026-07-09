0650c237d6ee1ee0f5f272da233a67cfdc2636d6
Apparats <raidermonsax@gmail.com>
2026-07-08 20:05:57 -0400

    Main menu new music plus lolflame2 buff

--- expansions/base/Lolflame2.js
+++ expansions/base/Lolflame2.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "Lolflame",
-  cost: 7,
+  cost: 5,
   type: "minion",
   attack: 4,
   health: 6,
--- public/audioConfig.js
+++ public/audioConfig.js
@@ -9,7 +9,9 @@ window.ArcaneAudioConfig = {
     mainMenu: {
       src: [
         "music/MainMenu1.ogg",
-        "music/MainMenu2.ogg",
+        "music/mainMenu2.ogg",
+        "music/mainMenu3.ogg",
+        "music/mainMenu4.ogg",
       ],
       loop: true,
       volume: 1,
