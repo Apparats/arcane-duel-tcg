8aac7eb2354c868c4b650c676270cb74f4b3a516
Apparats <raidermonsax@gmail.com>
2026-07-09 15:56:46 -0400

    New cards

--- expansions/base/Lolflame2.js
+++ expansions/base/Lolflame2.js
@@ -2,12 +2,14 @@ module.exports = {
   name: "Lolflame",
   cost: 5,
   type: "minion",
-  attack: 4,
+  attack: 3,
   health: 6,
   keywords: ["charge"], 
   race: "Monster",
   rarity: "mythic",
   country: "Djibouti",
-  lore: "This card can instantly attack when prepared.",
+  lore: "This card can instantly attack when prepared. This card also deals +1 damage to all enemy cards.",
   image: "art/Lolflames.webp",
+  abilities: [
+    { trigger: "onAttackMinion", effect: "damageAllEnemyMinions", value: 1 },
 };
---
+++ expansions/base/Zugzwang.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "Zugzwang",
+  cost: 5,
+  type: "minion",
+  attack: 3,
+  health: 6,
+  keywords: ["charge"], 
+  race: "Human",
+  rarity: "mythic",
+  country: "Bolivia",
+  lore: "I'm too sick for a pill! This card can instantly attack when prepared. This card also draws 3 more cards when played.",
+  image: "art/Zugzwang.webp",
+  abilities: [
+    { trigger: "onPlay", effect: "draw", value: 3 },
+};
