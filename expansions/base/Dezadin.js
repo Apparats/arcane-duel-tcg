f47a1cca8d667e2abb55e30ecb0f33e3199b9c61
Apparats <raidermonsax@gmail.com>
2026-07-09 22:49:19 -0400

    New card

---
+++ expansions/base/Dezadin.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "Dezadin",
+  cost: 7,
+  type: "minion",
+  attack: 4,
+  health: 7,
+  keywords: ["charge"], 
+  race: "Monster",
+  rarity: "mythic",
+  country: "Bolivia",
+  lore: "You’re made of spare parts, aren’t you, bud?. This card buffs all allied cards by +2",
+  image: "art/Zugzwang.webp",
+  abilities: [
+    { trigger: "onPlay", effect: "buffAllFriendlyMinions", attack: 2, health: 2 },
+  ],
+};
