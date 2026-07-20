093287d74f01ad1be556817eb521748486ce9210
Apparats <raidermonsax@gmail.com>
2026-07-20 18:26:00 -0400

    Fix

--- expansions/Expansion2/Michiel_op_Snuifari.js
+++ expansions/Expansion2/Michiel_op_Snuifari.js
@@ -1,13 +1,13 @@
 module.exports = {
   name: "Michiel_op_Snuifari",
-  cost: 1,
+  cost: 5,
   type: "minion",
-  attack: 1,
-  health: 1,
+  attack: 4,
+  health: 12,
   keywords: [],
   race: "Human",
-  rarity: "common",
-  country: "Arcana",
+  rarity: "mythical",
+  country: "Nigeria",
   lore: "While this is on your board, allied minions that die afterward revive with 1 Health and half their Attack. This does not affect Michiel_op_Snuifari.",
   image: "art/Michiel_op_Snuifari.webp",
   abilities: [
